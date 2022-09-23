5568919b521cd50fb4c4612cd3bf7b0ec204d0b4
Kosta K <76045173+kosta1221@users.noreply.github.com>
2022-09-23 14:07:43 +0300

    basic layout + NavBar

---
+++ constant/index.tsx
@@ -0,0 +1,44 @@
+import { SiGmail } from "react-icons/si";
+import { FaLinkedin, FaMedium, FaGithub } from "react-icons/fa";
+
+const menuLinks = [
+  { name: "About Me", route: "/about" },
+  { name: "Github", route: "/github" },
+  { name: "Articles", route: "/article" },
+  { name: "Contact", route: "/contact" },
+];
+
+const siteConfig = {
+  copyright: `Copyright © ${new Date().getFullYear()} Kosta K. All Rights Reserved.`,
+  author: {
+    name: "Kosta K",
+    accounts: [
+      {
+        url: process.env.GITHUB_URL,
+        icon: <FaGithub />,
+        name: "Github",
+        type: "gray",
+      },
+      {
+        url: process.env.LINKEDIN_URL,
+        icon: <FaLinkedin />,
+        name: "Linkedin",
+        type: "linkedin",
+      },
+      {
+        url: process.env.MEDIUM_URL,
+        icon: <FaMedium />,
+        name: "Medium",
+        type: "gray",
+      },
+      {
+        url: process.env.MAILTO_URL,
+        icon: <SiGmail />,
+        name: "Gmail",
+        type: "red",
+      },
+    ],
+  },
+};
+
+export { menuLinks, siteConfig };
